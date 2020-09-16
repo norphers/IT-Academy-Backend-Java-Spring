9cabf8e8c2cb0374205985b9e2d60916836e50bd
ocanades <ocanades@gmail.com>
2020-09-16 11:49:24 +0200

    exercise 2 java loops and maps

--- exer-2/src/exer2/Stage1.java
+++ exer-2/src/exer2/Stage1.java
@@ -2,8 +2,18 @@ package exer2;
 
 public class Stage1 {
 
+	static char name[] = {'j','o','h','n'};
+	
 	public static void main(String[] args) {
+		
+		loopThroughArray(name);
 
 	}
 
+	public static void loopThroughArray(char[] array) {
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
 }
